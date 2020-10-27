21217668c7c00a1b63c9448416866a3a2a669eae
AKHIL_NIBBI <akhil.nibbi@gmail.com>
2020-10-27 21:08:01 +0530

    adding file into jenkins

---
+++ prac1.py
@@ -0,0 +1,11 @@
+#practising jenkins
+def even_odd(num):
+    """to know if num is even or odd"""
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+even_odd(21)
+even_odd(66)
+
