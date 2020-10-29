489ed9266ce6ea3dc49f2c7670c3f3161a96a6b8
AKHIL_NIBBI <akhil.nibbi@gmail.com>
2020-10-30 00:12:34 +0530

    second commit

---
+++ pp2.py
@@ -0,0 +1,13 @@
+#practising jenkins
+def even_odd(num):
+    """to know if num is even or odd"""
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+even_odd(21)
+even_odd(66)
+even_odd(100)
+
+
